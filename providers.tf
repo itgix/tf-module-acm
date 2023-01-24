dd611a2fba011a5127c9fc53434ba7484f49d973
aalexiev <alexander.alexiev@itgix.com>
2023-01-24 12:11:12 +0200

    Removing provider version fix

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.63"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.63"
+#    }
+#  }
+#}
