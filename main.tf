0b0309eb49cd9ba15de1ee652c6e5148e54bc995
Victoria Dimitrova <victoria.dimitrova@itgix.com>
2026-04-15 15:00:17 +0300

    Fix cf_alias

--- main.tf
+++ main.tf
@@ -21,18 +21,18 @@ resource "aws_route53_record" "cert_validation" {
   for_each = { for domain, zone_id in local.domain_names : domain => zone_id if zone_id != "" }
 
   allow_overwrite = true
-  name            = tolist(aws_acm_certificate.cert[each.key].domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.cert[each.key].domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.cert[each.key].domain_validation_options)[0].resource_record_type
+  name            = tolist(aws_acm_certificate.cf_alias[each.key].domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.cf_alias[each.key].domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.cf_alias[each.key].domain_validation_options)[0].resource_record_type
   zone_id         = each.value
   ttl             = 60
 
-  depends_on = [aws_acm_certificate.cert]
+  depends_on = [aws_acm_certificate.cf_alias]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   for_each = { for domain, zone_id in local.domain_names : domain => zone_id if zone_id != "" }
 
-  certificate_arn         = aws_acm_certificate.cert[each.key].arn
+  certificate_arn         = aws_acm_certificate.cf_alias[each.key].arn
   validation_record_fqdns = [aws_route53_record.cert_validation[each.key].fqdn]
 }
